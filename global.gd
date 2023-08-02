aefb0f61b5cba0384017226edbc3e51ab8cc4b25
Leonardo Miranda Sousa da Silva <leo.fssmiranda@gmail.com>
2023-08-02 15:56:59 -0300

    Shop e Dicionário

---
+++ global.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var  polen = 0
+
+var itens = {
+	0:{
+		"Name" : "guerreira",
+		"Des" : "Uma abelha brava capaz de defender a colmeia de vespas",
+		"Cost" : 10
+	},
+	1:{
+		"Name" : "operaria",
+		"Des" : "uma abelha operaria que fornece polen com o tempo",
+		"Cost" : 5
+	},
+	2:{
+		"Name" : "construir colmeia",
+		"Des" : "construa uma outra colônia de abelhas!",
+		"Cost" : 100
+	},
+}
